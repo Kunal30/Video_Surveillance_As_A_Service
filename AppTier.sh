414790e81ef7e5e71a9c57aa7dca39cce1379734
AdityaGovardhan <dtgovardhan@gmail.com>
2019-03-27 18:25:08 -0700

    project submit

--- AppTier.sh
+++ AppTier.sh
@@ -7,8 +7,15 @@ echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | 
 sudo apt-get install -y oracle-java8-installer
 sudo apt-get install -y maven
 sudo apt install -y xvfb
+
+cd AppTier
 mvn clean package
-mv ./target/AppTier-1.0.0.jar ./darknet
-cd darknet
+mv ./target/AppTier-1.0.0.jar ~/darknet
+
+cd ../AppTier_Terminator
+mvn clean package
+mv ./target/AppTier_Terminator-1.0.0.jar ~/darknet
+
+cd ../darknet
 Xvfb :1 & export DISPLAY=:1
 java -jar AppTier-1.0.0.jar
--- infrastructure.sh
+++ infrastructure.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-vs_s3_bucket_name="vs-result-bucket-aditya"
+vs_s3_bucket_name="vs-result-bucket-adi"
 
-vs_input_queue_name="vs_input_queue-aditya"
-vs_output_queue_name="vs_output_queue-aditya"
+vs_input_queue_name="vs_input_queue"
+vs_output_queue_name="vs_output_queue"
 
-vs_vpc_name="vs_vpc-aditya"
-vs_internet_gateway_name="vs_internet_gateway-aditya"
-vs_subnet_name="vs_subnet-aditya"
-vs_security_group_name="vs_security_group-aditya"
-vs_key_pair_name="vs_key_pair-aditya"
+vs_vpc_name="vs_vpc"
+vs_internet_gateway_name="vs_internet_gateway"
+vs_subnet_name="vs_subnet"
+vs_security_group_name="vs_security_group"
+vs_key_pair_name="vs_key_pair"
 
-vs_instance_profile_name="vs_instance_profile-aditya"
-vs_role_name="vs_role-aditya"
+vs_instance_profile_name="vs_instance_profile"
+vs_role_name="vs_role"
 
-vs_web_instance_name="web_instance-aditya"
-vs_app_instance_name="app_instance_1-aditya"
+vs_web_instance_name="web_instance"
+vs_app_instance_name="app_instance_1"
 
-vs_app_instance_ami_name="vs_app_instance_ami-aditya"
+vs_app_instance_ami_name="vs_app_instance_ami"
 
 vs_vpc_cidr_block="10.0.0.0/16"
 vs_subnet_cidr_block="10.0.0.0/24"
@@ -39,7 +39,7 @@ if [ "$1" == "create" ]; then
 	echo 'creating S3 bucket...'
 	aws_region=`aws configure get region`
 	s3_bucket_url=`aws s3api create-bucket --bucket $vs_s3_bucket_name --region $aws_region --create-bucket-configuration LocationConstraint=$aws_region --query 'Location' --output text`
-	echo "vs_s3_bucket_url=$s3_bucket_url" > aws-resources.properties
+	echo "vs_s3_bucket_name=$vs_s3_bucket_name" > aws-resources.properties
 
 	echo 'creating SQS input queue...'
 	input_queue_url=`aws sqs create-queue --queue-name $vs_input_queue_name --query 'QueueUrl' --output text`
@@ -76,7 +76,7 @@ if [ "$1" == "create" ]; then
 	echo 'creating security group...'
 	security_group_id=`aws ec2 create-security-group --group-name $vs_security_group_name --description $vs_security_group_name --vpc-id $vpc_id --query 'GroupId' --output text`
 	aws ec2 create-tags --resources $security_group_id --tags "Key=\"Name\",Value=\"$vs_security_group_name\""
-	echo "vs_security_group_name=$vs_security_group_name" >> aws-resources.properties
+	echo "vs_security_group_id=$security_group_id" >> aws-resources.properties
 
 	echo 'allowing incoming traffic...'
 	aws ec2 authorize-security-group-ingress --group-id $security_group_id --protocol tcp --port 22 --cidr 0.0.0.0/0
@@ -100,6 +100,7 @@ if [ "$1" == "create" ]; then
 	aws iam attach-role-policy --role-name $vs_role_name --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
 	aws iam attach-role-policy --role-name $vs_role_name --policy-arn arn:aws:iam::aws:policy/AmazonSQSFullAccess
 	aws iam attach-role-policy --role-name $vs_role_name --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess
+	aws iam attach-role-policy --role-name $vs_role_name --policy-arn arn:aws:iam::aws:policy/IAMFullAccess
 
 	echo 'attaching role to instance profile...'
 	aws iam add-role-to-instance-profile --instance-profile-name $vs_instance_profile_name --role-name $vs_role_name
@@ -124,16 +125,28 @@ elif [ "$1" == "deploy-project" ]; then
 
 	vpc_id=`aws ec2 describe-vpcs --filters Name=tag:Name,Values=$vs_vpc_name  --query 'Vpcs[0].VpcId' --output text`
 
-	# echo "###################################"
-	# echo "###### DEPLOYING APP-TIER #########"
-	# echo "###################################"
+	# # echo "###################################"
+	# # echo "###### DEPLOYING APP-TIER #########"
+	# # echo "###################################"
+
+	cp aws-resources.properties ./AppTier/src/main/resources
+	cp aws-resources.properties ./AppTier_Terminator/src/main/resources
+
 	app_instance_ip=`aws ec2 describe-instances --filters Name=tag:Name,Values=$vs_app_instance_name Name=vpc-id,Values=$vpc_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text`
+	ssh    -i ~/.ssh/$vs_key_pair_name.pem -o StrictHostKeyChecking=no ubuntu@$app_instance_ip <<-HERE
+		mkdir AppTier
+		mkdir AppTier_Terminator
+		exit
+	HERE
 	scp    -i ~/.ssh/$vs_key_pair_name.pem -o StrictHostKeyChecking=no    darknet_test.py ubuntu@$app_instance_ip:~/darknet/
 	scp    -i ~/.ssh/$vs_key_pair_name.pem -o StrictHostKeyChecking=no    yolov3-tiny.weights ubuntu@$app_instance_ip:~/darknet/
-	scp    -i ~/.ssh/$vs_key_pair_name.pem -o StrictHostKeyChecking=no    ./AppTier/pom.xml ubuntu@$app_instance_ip:~/
-	scp -r -i ~/.ssh/$vs_key_pair_name.pem -o StrictHostKeyChecking=no    ./AppTier/src ubuntu@$app_instance_ip:~/
+	scp    -i ~/.ssh/$vs_key_pair_name.pem -o StrictHostKeyChecking=no    ./AppTier/pom.xml ubuntu@$app_instance_ip:~/AppTier/
+	scp -r -i ~/.ssh/$vs_key_pair_name.pem -o StrictHostKeyChecking=no    ./AppTier/src ubuntu@$app_instance_ip:~/AppTier/
+	scp -r -i ~/.ssh/$vs_key_pair_name.pem -o StrictHostKeyChecking=no    ./AppTier_Terminator/pom.xml ubuntu@$app_instance_ip:~/AppTier_Terminator/
+	scp -r -i ~/.ssh/$vs_key_pair_name.pem -o StrictHostKeyChecking=no    ./AppTier_Terminator/src ubuntu@$app_instance_ip:~/AppTier_Terminator/
 	scp    -i ~/.ssh/$vs_key_pair_name.pem -o StrictHostKeyChecking=no    AppTier.sh ubuntu@$app_instance_ip:~/
 	ssh    -i ~/.ssh/$vs_key_pair_name.pem -o StrictHostKeyChecking=no -t ubuntu@$app_instance_ip bash AppTier.sh &
+	
 	# ssh    -i ~/.ssh/$vs_key_pair_name.pem -o StrictHostKeyChecking=no ubuntu@$app_instance_ip <<-HERE
 	# 	sudo add-apt-repository -y ppa:webupd8team/java
 	# 	sudo apt-get update
@@ -152,8 +165,10 @@ elif [ "$1" == "deploy-project" ]; then
 	# echo "########################################"
 	# echo "###### CREATING APP-TIER IMAGE #########"
 	# echo "########################################"
-	sleep 20 # TODO: needed?
+	
+	sleep 120
 
+	# put app tier terminating in this
 	app_instance_id=`aws ec2 describe-instances --filters Name=tag:Name,Values=$vs_app_instance_name Name=vpc-id,Values=$vpc_id --query 'Reservations[0].Instances[0].InstanceId' --output text`
 	app_instance_ami_id=`aws ec2 create-image --instance-id $app_instance_id --name $vs_app_instance_ami_name --no-reboot --query 'ImageId' --output text`
 	aws ec2 create-tags --resources $app_instance_ami_id --tags "Key=\"Name\",Value=\"$vs_app_instance_ami_name\""
@@ -210,6 +225,7 @@ elif [ "$1" == "destroy" ]; then
 	aws iam detach-role-policy --role-name $vs_role_name --policy-arn arn:aws:iam::aws:policy/AmazonS3FullAccess
 	aws iam detach-role-policy --role-name $vs_role_name --policy-arn arn:aws:iam::aws:policy/AmazonSQSFullAccess
 	aws iam detach-role-policy --role-name $vs_role_name --policy-arn arn:aws:iam::aws:policy/AmazonEC2FullAccess
+	aws iam detach-role-policy --role-name $vs_role_name --policy-arn arn:aws:iam::aws:policy/IAMFullAccess
 
 	echo 'deleting role...'
 	aws iam delete-role --role-name $vs_role_name
